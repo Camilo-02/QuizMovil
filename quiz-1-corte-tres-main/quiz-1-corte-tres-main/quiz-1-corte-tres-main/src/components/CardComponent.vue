23280ba5706f9e1c7041487508f711e37874eeb8
Camilo-02 <camiloandreschavarroguenis@gmail.com>
2024-04-24 10:08:38 -0500

    quiz 1.1

---
+++ quiz-1-corte-tres-main/quiz-1-corte-tres-main/quiz-1-corte-tres-main/src/components/CardComponent.vue
@@ -0,0 +1,43 @@
+<template>
+            <ion-card :id="id" :color="color">
+                <ion-card-header>
+                    <ion-card-title>{{value}}</ion-card-title>
+                    <ion-card-subtitle>{{subtitulo}}</ion-card-subtitle>
+                    </ion-card-header>
+
+                    <ion-card-content>
+                    {{ content }}
+                    </ion-card-content>            
+            </ion-card>
+
+</template>
+  
+  <script lang="ts">
+  import { IonCard } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    components: { IonCard },
+    props: {
+      subtitulo: String,
+      id: {
+        type: String
+      },
+      value: {
+        type: String
+      },
+
+      
+      
+      color: String,
+      header: String,
+      title: String,
+      subtitle: String,
+      content: String
+    }
+  });
+  </script>
+  
+  <style scoped>
+
+  </style>
---
+++ quiz-1-corte-tres-main/quiz-1-corte-tres-main/quiz-1-corte-tres-main/src/views/Tab1Page.vue
@@ -0,0 +1,47 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Quiz</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Vista</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+
+      <InputComponent id = "Nombre" value = "Ingresa Nombre" label = "Nombre:" type = "text"></InputComponent>
+      <InputComponent id = "Fecha" label = "Fecha Nacimiento:" type= "date" ></InputComponent>
+      <InputComponent id = "Celular" value = "#" label = "Número:" type = "text"></InputComponent>
+      <InputComponent id = "Correo" value = "Correo Electronico" label = "Correo:" type = "text"></InputComponent>
+      <ion-list>
+
+        
+
+<ButtonComponent id = "btnAgregar" value="agregar"/>
+<ButtonComponent id = "btnModificar" value="modificar"/>
+<ButtonComponent id = "btnEliminar" value="eliminar"  color = "danger"/>
+<ButtonComponent id = "btnConsultar" value="consultar"/>
+
+
+
+  </ion-list>
+  <div>
+    <CardComponent id = "mensaje" value = "Caja de Mensaje" subtitulo="Escribenos tu opinion" content= "" ></CardComponent>
+  </div>
+  
+    </ion-content>
+  </ion-page>
+
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ButtonComponent from '@/components/ButtonComponent.vue';
+import InputComponent from '@/components/InputComponent.vue';
+import CardComponent from '@/components/CardComponent.vue';
+
+</script>
