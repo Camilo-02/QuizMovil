b7be38860aca4e4cf5ce4f05345ac356af461a97
Camilo-02 <camiloandreschavarroguenis@gmail.com>
2024-04-24 08:58:07 -0500

    front

---
+++ FrontEnd/front-end-parcial/src/views/Tab1Page.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Cliente</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Login</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div id="login-container">
+
+      
+        <div id="login-form">
+          <InputComponent id="nombre" name="nombre" label="Nombre: " />
+          <InputComponent type="email" id="email" name="email" label="Correo: " />
+          <ButtonComponent id="login" value="Login" fill="outline" />
+        </div>
+
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import InputComponent from '@/components/InputComponent.vue';
+import ButtonComponent from '@/components/ButtonComponent.vue';
+</script>
---
+++ FrontEnd/front-end-parcial/src/views/Tab2Page.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Evento</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">login</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div id="login-container">
+
+      
+        <div id="login-form">
+          <InputComponent id="nombre" name="nombre" label="Nombre del Evento: " />
+          <InputComponent type="date" id="fecha" name="fecha" label="Fecha: " />
+          <ButtonComponent id="agendar" value="Agendar" fill="outline" />
+        </div>
+
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import InputComponent from '@/components/InputComponent.vue';
+import ButtonComponent from '@/components/ButtonComponent.vue';
+</script>
